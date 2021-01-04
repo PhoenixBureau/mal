d6a6f950675848d3217607f30435b62384eade7d
Simon Forman <sforman@hushmail.com>
2021-01-03 16:27:30 -0800

    Handle Ctrl-C and EOF for graceful exit.

--- impls/nim-too/step0_repl.nim
+++ impls/nim-too/step0_repl.nim
@@ -6,6 +6,19 @@ proc eval(ast: string): string = ast
 
 proc print(exp: string): string = exp
 
+proc rep(input: string): string =
+  print(eval(read(input)))
+
+
+# Handle Ctrl-C by raising an IOError to break out of the mainloop
+# without waiting for the user to press enter.
+proc ctrlc() {.noconv.} =
+  raise newException(IOError, "Got Ctrl-C, bye!")
+setControlCHook(ctrlc)
+
+
 while true:
-  let line = readLineFromStdin("user> ")
-  echo line.read.eval.print
+  try:
+    echo rep(readLineFromStdin("user> "))
+  except IOError:
+    break
