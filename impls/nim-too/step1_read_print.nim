95b9ee515c73f8ccc8565d8f812db42299640ca1
Simon Forman <sforman@hushmail.com>
2021-01-03 18:14:13 -0800

    Starting on step 1.

---
+++ impls/nim-too/step1_read_print.nim
@@ -0,0 +1,24 @@
+import rdstdin
+
+proc read(str: string): string = str
+
+proc eval(ast: string): string = ast
+
+proc print(exp: string): string = exp
+
+proc rep(input: string): string =
+  print(eval(read(input)))
+
+
+# Handle Ctrl-C by raising an IOError to break out of the mainloop
+# without waiting for the user to press enter.
+proc ctrlc() {.noconv.} =
+  raise newException(IOError, "Got Ctrl-C, bye!")
+setControlCHook(ctrlc)
+
+
+while true:
+  try:
+    echo rep(readLineFromStdin("user> "))
+  except IOError:
+    break
